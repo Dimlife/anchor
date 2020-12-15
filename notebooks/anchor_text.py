7c2127cbd24896bf7c5e1152a22fc83862bb6266
unknown <jinzhiyu@bilibili.com>
2020-12-15 16:40:58 +0800

    test data and labels

--- notebooks/anchor_text.py
+++ notebooks/anchor_text.py
@@ -27,7 +27,7 @@ def load_polarity(path='./rt-polaritydata'):
 
 nlp = spacy.load('/home/jinzhiyu/anchor/en_core_web_lg-3.0.0a0/en_core_web_lg/en_core_web_lg-3.0.0a0')
 
-data, labels = ['你好 上海', '你好 杨浦', '你好 北京', '你不好好 哥哥'], [1, 1, 1, 0]
+data, labels = ['你好 上海', '你好 杨浦', '你好 北京', '你不好好 哥哥', '你好 菜鸟', '你好 不夜城', '你不好 不夜城', '你不好 哥', '你不好 背景'], [1, 1, 1, 0, 1, 1, 0, 0, 0]
 # data, labels = load_polarity()
 print(data, labels)
 train, test, train_labels, test_labels = sklearn.model_selection.train_test_split(data, labels, test_size=.2, random_state=42)
