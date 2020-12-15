a74a9d2bdaae08c898de303f38d52c888237b1f5
unknown <jinzhiyu@bilibili.com>
2020-12-15 14:03:14 +0800

    test

---
+++ notebooks/anchor_text.py
@@ -0,0 +1,30 @@
+import os
+import os.path
+import numpy as np
+import sklearn
+import sklearn.model_selection
+import sklearn.linear_model
+import sklearn.ensemble
+import spacy
+import sys
+from sklearn.feature_extraction.text import CountVectorizer
+from anchor import anchor_text
+import time
+
+def load_polarity(path='./rt-polaritydata'):
+    data = []
+    labels = []
+    f_names = ['rt-polarity.neg', 'rt-polarity.pos']
+    for (l, f) in enumerate(f_names):
+        for line in open(os.path.join(path, f), 'rb'):
+            try:
+                line.decode('utf8')
+            except:
+                continue
+            data.append(line.strip())
+            labels.append(l)
+    return data, labels
+
+nlp = spacy.load('C:/Users/jinzhiyu/Desktop/zh_core_web_trf-3.0.0a0/zh_core_web_trf/zh_core_web_trf-3.0.0a0')
+
+print(nlp)
