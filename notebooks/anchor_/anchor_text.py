a04df7f63d8c6d0824b027a5395ba9299f2c0fe9
unknown <jinzhiyu@bilibili.com>
2020-12-16 12:04:45 +0800

    prepared for masked-lm bert

--- notebooks/anchor_/anchor_text.py
+++ notebooks/anchor_/anchor_text.py
@@ -25,14 +25,14 @@ def exp_normalize(x):
 
 class TextGenerator(object):
     def __init__(self, url=None):
-        from transformers import DistilBertForMaskedLM, DistilBertTokenizer
+        from transformers import BertForMaskedLM, BertTokenizer
         import torch
         self.torch = torch
         self.url = url
         if url is None:
             self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-            self.bert_tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-chinese')
-            self.bert = DistilBertForMaskedLM.from_pretrained('distilbert-base-chinese')
+            self.bert_tokenizer = BertTokenizer.from_pretrained('bert-base-chinese')
+            self.bert = BertForMaskedLM.from_pretrained('bert-base-chinese')
             self.bert.to(self.device)
             self.bert.eval()
 
