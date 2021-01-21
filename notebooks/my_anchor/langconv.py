9de877a59708aa7ec19f18bfa206bd02111e97e4
unknown <jinzhiyu@bilibili.com>
2021-01-21 16:57:28 +0800

    deal with import issue

--- notebooks/my_anchor/langconv.py
+++ notebooks/my_anchor/langconv.py
@@ -22,7 +22,7 @@ if py3k:
     UEMPTY = ''
 else:
     _zh2Hant, _zh2Hans = {}, {}
-    for old, new in ((zh2Hant, _zh2Hant), (zh2Hans, _zh2Hans)):
+    for old, new in ((zh_wiki.zh2Hant, _zh2Hant), (zh_wiki.zh2Hans, _zh2Hans)):
         for k, v in old.items():
             new[k.decode('utf8')] = v.decode('utf8')
     zh2Hant = _zh2Hant
